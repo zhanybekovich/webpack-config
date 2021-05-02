ecad820bffb89a455ef7bcf958843220f8453970
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2021-05-02 11:01:34 +0600

    add babel

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ const elvenShieldRecipe = {
   ironIngot: 0
 }
 
+const elvenGauntletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonStone: 4,
+}
+
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
